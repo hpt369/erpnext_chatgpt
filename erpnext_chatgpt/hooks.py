91fb66e184b28c6e59c47bbc0275e8ba75896fe3
hpt369 <gvina805@gmail.com>
2025-05-29 01:48:21 +0530

    Update hooks.py

--- erpnext_chatgpt/hooks.py
+++ erpnext_chatgpt/hooks.py
@@ -1,10 +1,10 @@
 app_name = "erpnext_chatgpt"
 app_title = "OpenAI Integration"
-app_publisher = "William Luke"
+app_publisher = "Vinnr"
 app_description = "ERPNext app for OpenAI integration"
 app_icon = "octicon octicon-file-directory"
 app_color = "grey"
-app_email = "williamluke4@gmail.com"
+app_email = "v"
 app_license = "MIT"
 
 # Include JS and CSS files in header of desk.html
@@ -16,3 +16,8 @@ doctype_js = {
 }
 
 fixtures = [{"dt": "DocType", "filters": [["name", "in", ["OpenAI Settings"]]]}]
+doc_events = {
+    "Communication": {
+        "after_insert": "erpnext_chatgpt.auto_email.reply_to_incoming"
+    }
+}
